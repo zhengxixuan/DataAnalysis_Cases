bbadb12670b44a4d144e29a39dcd2febcbab153f
zhengxixuan <zhengxixuan1020@gmail.com>
2016-12-15 10:58:27 +0800

    update

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+# you can change the first/second/third for and words.
