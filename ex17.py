b78180258e595aa2e4ee0bf3f27a35649cdefc0f
zhengxixuan <zhengxixuan1020@gmail.com>
2016-12-18 13:50:19 +0800

    ex18

--- ex17.py
+++ ex17.py
@@ -5,7 +5,7 @@ script, from_file, to_file = argv
 
 print "Copying from %s to %s" % (from_file, to_file)
 
-# we could do these two on one line, how?
+# we could do these two on one line, how? ";" 
 in_file = open(from_file)
 indata = in_file.read()
 
