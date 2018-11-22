49f7b4c0f34f1ceb40b8922ec566ecf004f19e8e
zhengxixuan <zhengxixuan1020@gmail.com>
2018-11-23 00:00:58 +0800

    add ex26-28

---
+++ 01LPTHW/ex27.py
@@ -0,0 +1,15 @@
+# This ex start learning about logic.
+
+# and
+# or
+# not
+# != (not equal)
+# == (equal)
+# >= (greater-than-equal)
+# <= (less-than-equal)
+# True
+# False
+
+# True or False --> True
+# True and False --> False
+
