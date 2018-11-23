dc77cf9639aca724b1626942bd0f5e5877375d3a
zhengxixuan <zhengxixuan1020@gmail.com>
2018-11-23 23:28:07 +0800

    add ex29-35

---
+++ 01LPTHW/ex34.py
@@ -0,0 +1,6 @@
+# animals = ['bear', 'python3.7', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The first animal. --> The first animal is at 0 and is a bear.
+# The animal at 1. --> The animal at 1 is the second animal and is python3.7
+# The third animal. --> peacock
+# The animal at 3. --> kangaroo
